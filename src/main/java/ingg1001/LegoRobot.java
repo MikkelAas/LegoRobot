145fc52231573a389adc253c17ce11111e8541fe
Jakob <jakob.karlsmoen@hotmail.no>
2019-11-18 13:28:32 +0100

    Updated all comments and Javadoc

--- src/main/java/ingg1001/LegoRobot.java
+++ src/main/java/ingg1001/LegoRobot.java
@@ -22,39 +22,66 @@ import lejos.utility.Delay;
 
 public class LegoRobot {
 
-    // Making objects of the hardware in use
+    // hardware
     private static final EV3LargeRegulatedMotor largeRegulatedMotor = new EV3LargeRegulatedMotor(MotorPort.A);
     private static final EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S1);
     private static final EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S2);
     private static final EV3MediumRegulatedMotor mediumRegulatedMotor = new EV3MediumRegulatedMotor(MotorPort.B);
+
+    // robot
     private static final LegoRobot robot = new LegoRobot();
 
+    // color array
     private static final String[] colors = {"NONE", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "WHITE", "BROWN"};
 
+    /**
+     * Enum containing the values for how far the sorter should move for each color
+     */
     private enum MoveDistances {
+        // sets the colors and their respective move distances
         BLUE(0),
         GREEN(150),
         YELLOW(450),
         RED(750);
 
+        // moveDistance field
         private final int moveDistance;
 
+        /**
+         * Constructor for enum to store the distance the sorter should move depending on color
+         * @param moveDistance The value that defines the amount of time the sorter should move
+         */
         MoveDistances(int moveDistance) {
             this.moveDistance = moveDistance;
         }
 
+        /**
+         * Method in enum to return the defined move distance for the different colors
+         * @return The distance for the sorter to move as int
+         */
         public int getMoveDistance() {
             return this.moveDistance;
         }
     }
 
+    /**
+     * Initializes robot by resetting all positions
+     */
     public void init() {
         robot.resetDispenserRotation();
         robot.resetSorterPosition();
     }
 
+    /**
+     * Method for moving the sorter
+     * @param speed Sets the speed for how fast the sorter should move
+     * @param directionForwards Sets which direction to move in
+     */
     public void moveSorter(int speed, boolean directionForwards) {
+        // sets speed for sorter
         largeRegulatedMotor.setSpeed(speed);
+
+        // move sorter, either forwards or backwards
         if (directionForwards) {
             largeRegulatedMotor.forward();
         } else {
@@ -62,30 +89,54 @@ public class LegoRobot {
         }
     }
 
+    /**
+     * Method for moving the sorter to the start position
+     */
     public void resetSorterPosition() {
-        // go back to start
+        // move the sorter towards it's start position until reset button is pressed
         while (!touchSensor.isPressed()) {
             robot.moveSorter(600, false);
         }
+
+        // stop the motor
         largeRegulatedMotor.stop();
     }
 
+    /**
+     * Method for resetting the dispenser mechanism
+     */
     public void resetDispenserRotation() {
+        // rotate the dispenser mechanism towards it's start position until it is stalled
         while (!mediumRegulatedMotor.isStalled()) {
             mediumRegulatedMotor.setSpeed(150);
             mediumRegulatedMotor.forward();
         }
+
+        // stop the motor
         mediumRegulatedMotor.stop();
     }
 
+    /**
+     * Method for dispensing a single color block
+     */
     public void dispense() {
+        // rotate the dispenser mechanism for a set time to dispense a color
         mediumRegulatedMotor.setSpeed(250);
         mediumRegulatedMotor.backward();
         Delay.msDelay(1000);
+
+        // stop the motor
         mediumRegulatedMotor.stop();
     }
 
+    /**
+     * Method for moving the sorter to the correct position and dispensing the color block
+     * @param delay Delay defining (in ms) how much time it should spend moving the sorter before dispensing a color
+     * @param currentColor Sets the current color getting sorted for the method to compare to the next
+     */
     public void sortColor(int delay, int currentColor) {
+        System.out.println("Sorting " + colors[currentColor] + "!");
+
         // move to dispense location
         robot.moveSorter(600, true);
         Delay.msDelay(delay);
@@ -102,8 +153,10 @@ public class LegoRobot {
         robot.resetDispenserRotation();
         mediumRegulatedMotor.stop();
 
+        // wait for new color to fall in place
         Delay.msDelay(250);
 
+        // read new color and compare to last, if equal dispense the new color block
         if (robot.getColorID() == currentColor) {
             System.out.println("Sorted double!");
 
@@ -123,16 +176,20 @@ public class LegoRobot {
         largeRegulatedMotor.stop();
     }
 
+    /**
+     * Retrieves the current color to be sorted
+     * @return The ID corresponding with the color read
+     */
     public int getColorID() {
         colorSensor.getColorIDMode();
 
-        // A float array that holds the samples
+        // a float array that is going to store the sample from the color sensor
         float[] colorSample = new float[colorSensor.sampleSize()];
 
-        // Returns a float from 0 to 7
+        // returns a color ID from 0-7 depending on color read
         colorSensor.fetchSample(colorSample, 0);
 
-        // Returns a String from the colors array based on what float it fetches, converted to int
+        // returns the color ID as an int
         return (int) colorSample[0];
     }
 
@@ -142,26 +199,23 @@ public class LegoRobot {
         robot.resetDispenserRotation();
         largeRegulatedMotor.stop();
 
-
         while (true) {
+            // read current color
             int currentColor = robot.getColorID();
 
+            // check read color and call sortColor method
             switch (currentColor) {
                 case 2: // BLUE
                     robot.sortColor(MoveDistances.BLUE.getMoveDistance(), currentColor);
-                    System.out.println(colors[currentColor]);
                     break;
                 case 3: // GREEN
                     robot.sortColor(MoveDistances.GREEN.getMoveDistance(), currentColor);
-                    System.out.println(colors[currentColor]);
                     break;
                 case 4: // YELLOW
                     robot.sortColor(MoveDistances.YELLOW.getMoveDistance(), currentColor);
-                    System.out.println(colors[currentColor]);
                     break;
                 case 5: // RED
                     robot.sortColor(MoveDistances.RED.getMoveDistance(), currentColor);
-                    System.out.println(colors[currentColor]);
                     break;
             }
         }
